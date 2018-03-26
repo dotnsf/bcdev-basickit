10cede325fe4ceeb4ada5a88b30fd419753a78da
K.Kimura <dotnsf@gmail.com>
2018-03-26 16:03:49 +0900

    use BlockChain(not cloudant) as default

--- api/settings.js
+++ api/settings.js
@@ -2,7 +2,7 @@ exports.cardName = 'admin@bcdev-basickit-network'; //. 'PeerAdmin@hlfv1';
 
 exports.superSecret = 'welovensf';
 
-exports.cloudant_db = 'bcdb'; //. Use cloudant if this value is not false
+exports.cloudant_db = ''; //. Use cloudant if this value is not false
 exports.cloudant_username = 'cloudant_username';
 exports.cloudant_password = 'cloudant_password';
 
